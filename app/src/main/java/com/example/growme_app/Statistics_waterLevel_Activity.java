b69f1a8998c43ed25857561b4ca275b442fb330a
karin <karin.papismadov@gmail.com>
2023-06-12 08:19:13 +0300

    update 11.06

--- app/src/main/java/com/example/growme_app/Statistics_waterLevel_Activity.java
+++ app/src/main/java/com/example/growme_app/Statistics_waterLevel_Activity.java
@@ -1,22 +1,56 @@
 package com.example.growme_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Statistics_waterLevel_Activity extends AppCompatActivity {
 
-    TextView water_AVG;
+    private TextView water_AVG;
+    Button back;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics_water_level);
 
         water_AVG = findViewById(R.id.waterAVG);
+        back = findViewById(R.id.button);
 
+        DatabaseReference waterLevelRef = FirebaseDatabase.getInstance().getReference().child("CurrentData").child("Water level");
+
+        waterLevelRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String waterLevel = dataSnapshot.getValue(String.class);
+                water_AVG.setText(waterLevel);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Statistics_waterLevel_Activity.this, StatisticsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
