1f1e54b60ff827fe7db5ef126b9ab28f6237f6bf
karin <karin.papismadov@gmail.com>
2023-06-14 13:13:14 +0300

    update 14.06 user java

--- app/src/main/java/com/example/growme_app/UserActivity.java
+++ app/src/main/java/com/example/growme_app/UserActivity.java
@@ -1,14 +1,69 @@
 package com.example.growme_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class UserActivity extends AppCompatActivity {
 
-    @Override
+
+    TextView name, code, mail;
+    Button back;
+
+    @SuppressLint("WrongViewCast")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference userRef = database.getReference("Users");
+
+        name = findViewById(R.id.text_show_name);
+        code = findViewById(R.id.text_show_code);
+        mail = findViewById(R.id.text_show_email);
+
+        back = findViewById(R.id.user_back);
+
+        // Attach a ValueEventListener to retrieve the data from Firebase
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // Retrieve the metric values from the dataSnapshot
+                String nameU = dataSnapshot.child("name").getValue(String.class);
+                String mailU = dataSnapshot.child("email").getValue(String.class);
+                String codeU = dataSnapshot.child("code").getValue(String.class);
+
+
+                // Set the metric values to the TextViews
+                name.setText(nameU);
+                code.setText(mailU);
+                mail.setText(codeU);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle onCancelled event
+            }
+        });
+
+
+
+        //back button
+        back.setOnClickListener(view -> {
+            Intent intent = new Intent(UserActivity.this, MainMenuPageActivity.class);
+            startActivity(intent);
+        });
     }
 }
