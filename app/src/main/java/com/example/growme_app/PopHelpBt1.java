ae2752cab75d690857fa02e10d4ab781586d045e
karin <karin.papismadov@gmail.com>
2023-06-15 14:35:36 +0300

    update 14.06 help pop up messages java file

--- app/src/main/java/com/example/growme_app/PopHelpBt1.java
+++ app/src/main/java/com/example/growme_app/PopHelpBt1.java
@@ -2,13 +2,25 @@ package com.example.growme_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class PopHelpBt1 extends AppCompatActivity {
 
+    Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop_help_bt1);
+
+
+        back = findViewById(R.id.BACK_TO_HELP_PAGE);
+
+
+        back.setOnClickListener(view -> {
+            Intent intent = new Intent(PopHelpBt1.this, HelpActivity.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/example/growme_app/PopHelpBt3.java
+++ app/src/main/java/com/example/growme_app/PopHelpBt3.java
@@ -2,13 +2,24 @@ package com.example.growme_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class PopHelpBt3 extends AppCompatActivity {
-
+    Button back;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop_help_bt3);
+        back = findViewById(R.id.BACK_TO_HELP_PAGE);
+
+
+        back.setOnClickListener(view -> {
+            Intent intent = new Intent(PopHelpBt3.this, HelpActivity.class);
+            startActivity(intent);
+        });
     }
 }
