6d1dff120368467d8cbe6e9314958554ec6ea124
karin <karin.papismadov@gmail.com>
2023-06-15 12:25:54 +0300

    update 14.06 help pop up messages java file

--- app/src/main/java/com/example/growme_app/HelpActivity.java
+++ app/src/main/java/com/example/growme_app/HelpActivity.java
@@ -16,13 +16,23 @@ import android.widget.TextView;
 public class HelpActivity extends AppCompatActivity {
 
     Button Bt1, Bt2, Bt3, Bt4, Bt5, Bt6, Bt7, Bt8, back;
-
+    RelativeLayout parent;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
+        parent=findViewById(R.id.parent_layout);
+        Bt1 = findViewById(R.id.I_have_a_system_how_can_I_now_create_my_garden);
 
+
+        Bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupWindow1();
+            }
+        });
 
 
         back = findViewById(R.id.BACK_TO_MENU_PAGE);
@@ -33,4 +43,21 @@ public class HelpActivity extends AppCompatActivity {
             startActivity(intent);
         });
     }
+    private void showPopupWindow1(){
+        View view=View.inflate(this, R.layout.popup_layout, null);
+        Button close=view.findViewById(R.id.close_pop);
+        TextView body1=view.findViewById(R.id.popup_msg);
+        int width= LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height= LinearLayout.LayoutParams.WRAP_CONTENT;
+        PopupWindow popupWindow=new PopupWindow(view, width,height,false);
+        popupWindow.showAtLocation(parent, Gravity.CENTER, 0,0);
+        body1.setText(R.string.message1);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+            }
+        });
+    }
+
 }
